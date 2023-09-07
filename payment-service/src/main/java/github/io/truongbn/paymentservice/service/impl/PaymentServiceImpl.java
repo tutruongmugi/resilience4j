f129a4b2564e835b121cc15973e74328d4125498
tutruongmugi <khongthenho304@gmail.com>
2023-09-07 16:49:31 +0700

    add CircuitBreaker

--- payment-service/src/main/java/github/io/truongbn/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/github/io/truongbn/paymentservice/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,7 @@
 package github.io.truongbn.paymentservice.service.impl;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -14,12 +16,26 @@ import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class PaymentServiceImpl implements PaymentService {
     private final RestTemplate restTemplate;
     private static final String SERVICE_NAME = "payment-service";
     private static final String PAYMENT_PROCESSOR_URL = "http://localhost:1010/api/v1/processor-payment";
+//    @RateLimiter(name = SERVICE_NAME, fallbackMethod = "fallbackMethod")
+//    public Type submitPayment(String paymentInfo) {
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.setContentType(MediaType.APPLICATION_JSON);
+//        HttpEntity<String> entity = new HttpEntity<>(paymentInfo, headers);
+//        ResponseEntity<String> response = restTemplate.exchange(PAYMENT_PROCESSOR_URL,
+//                HttpMethod.POST, entity, String.class);
+//        Success success = new Success(response.getBody());
+//        return success;
+//    }
+
+    @CircuitBreaker(name = SERVICE_NAME, fallbackMethod = "fallbackMethod")
     @RateLimiter(name = SERVICE_NAME, fallbackMethod = "fallbackMethod")
     public Type submitPayment(String paymentInfo) {
+        log.info("check failed request");
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<String> entity = new HttpEntity<>(paymentInfo, headers);
@@ -29,7 +45,8 @@ public class PaymentServiceImpl implements PaymentService {
         return success;
     }
 
-    private Type fallbackMethod(RequestNotPermitted requestNotPermitted) {
+    private Type fallbackMethod(Exception e) {
+        log.info("check fallbackMethod : ", e.getMessage());
         return new Failure("Payment service does not permit further calls");
     }
 }
